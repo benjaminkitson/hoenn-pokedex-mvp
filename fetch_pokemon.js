ed6e61a1ab8a7f744dd50cd72dbe863caeb1ffa8
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-02 12:39:08 +0000

    clicking on pokemon carries out ajax request, inserts information into div

--- fetch_pokemon.js
+++ fetch_pokemon.js
@@ -1,5 +1,7 @@
 //Fetch pokemon script
 
+let info
+
 fetch("https://pokeapi.co/api/v2/pokemon?limit=386")
   .then(response => response.json())
   .then(data => data.results.forEach((pokemon) => {
@@ -21,3 +23,43 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=386")
     `)
   pokemonCounter++
   }))
+
+function showData() {
+  const id = this.getAttribute("data-id")
+  infoTab.innerHTML = ""
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then((data) => {
+
+      let letters = data.forms[0].name.split('')
+      letters[0] = letters[0].toUpperCase()
+      let pokemonName = letters.join('')
+
+      let letters2 = data.abilities[0].ability.name.split('')
+      letters2[0] = letters2[0].toUpperCase()
+      let pokemonAbility = letters2.join('')
+
+
+      console.log(data)
+
+    infoTab.insertAdjacentHTML("beforeend", `
+      <div class="pokemon-info">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png">
+        <div>Name: ${pokemonName}</div>
+        <div>Ability: ${pokemonAbility}</div>
+        <div>Height: ${data.height >= 10 ? `${(data.height / 10).toFixed(2)}m` : `${data.height * 10}cm`} </div>
+        <div>Weight: ${Math.ceil(data.weight / 10)}kg </div>
+      </div>
+    `)
+
+    })
+
+}
+
+setTimeout(() => {
+  pokemons = document.querySelectorAll('.pokemon');
+  pokemons.forEach((pokemon) => {
+    pokemon.addEventListener('click', showData)
+  })
+}, 100);

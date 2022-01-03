cabcfeb2218bf93fa532dec869007b672b786861
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-03 16:36:27 +0000

    rabbit-holed super hard and implemented everything without any commits - types added to pokemon, html is inserted with relevant classes added for colouring of types

--- fetch_pokemon.js
+++ fetch_pokemon.js
@@ -8,6 +8,7 @@ const pokemonAbility = document.querySelector('.pokemon-ability');
 const pokemonHeight = document.querySelector('.pokemon-height');
 const pokemonWeight = document.querySelector('.pokemon-weight');
 const pokemonDescription = document.querySelector('.pokemon-description');
+const pokemonTypes = document.querySelector('.type-content')
 
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=386")
@@ -79,7 +80,15 @@ function showData() {
         pokemonHeight.innerHTML = displayPokemon.height >= 100 ? `${(displayPokemon.height / 100).toFixed(2)}m` : `${displayPokemon.height}cm`
         pokemonWeight.innerHTML = displayPokemon.weight + "kg"
         pokemonDescription.innerHTML = (displayPokemon.description === undefined) ? "Description failed to load" : displayPokemon.description
-
+        pokemonTypes.innerHTML = ''
+        displayPokemon.types.forEach((type) => {
+          let letters = type.split('')
+          letters[0] = letters[0].toLowerCase()
+          let formattedType = letters.join('')
+          pokemonTypes.insertAdjacentHTML('beforeend', `
+            <div class="${formattedType} type">${type}</div>
+          `)
+        })
         }, 200);
       })
 

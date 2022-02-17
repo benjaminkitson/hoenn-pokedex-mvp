a71bc426c6ad9580dff0a9412e0d62ecccd2078f
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-02-17 15:12:25 +0000

    express app setup

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
-const app = require('express')
-const pokedex = require('./pokedex')
+const express = require('express');
+const pokedex = require('./pokedex');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+const PORT = pricess.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '..templates/views'))
+hbs.registerPartials(path.join(__dirname, '../templates/partials'))

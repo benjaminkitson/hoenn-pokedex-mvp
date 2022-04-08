98d1253067f201a4d579c10c08007bb58a3db220
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-08 15:51:41 +0100

    more react based tweaks, dev-server now starts without errors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Content from './components/Content';
-import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const app = document.getElementById('app');
